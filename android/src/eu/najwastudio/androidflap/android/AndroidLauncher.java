b443710a7a43f30f6affced94afffa6853985fa4
jacktech24 <jacktech24@gmail.com>
2015-05-27 11:13:31 +0200

    initial commit

---
+++ android/src/eu/najwastudio/androidflap/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package eu.najwastudio.androidflap.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import eu.najwastudio.androidflap.AndroidFlap;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AndroidFlap(), config);
+	}
+}
---
+++ desktop/src/eu/najwastudio/androidflap/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package eu.najwastudio.androidflap.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import eu.najwastudio.androidflap.AndroidFlap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AndroidFlap(), config);
+	}
+}
